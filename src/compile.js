6d6b715d65832feea89f8c817678523ac70c54f1
Charlytoc <charlyjchaconc@gmail.com>
2024-03-31 20:31:29 -0500

    updating compiler to return result object

--- src/compile.js
+++ src/compile.js
@@ -19,9 +19,16 @@ module.exports = {
       
       const header = fs.readFileSync(path.resolve(__dirname,'./utils/prepend.compile.js'), "utf8");
 
-      const result = await node.runSource(`${header} ${content}`, { stdin: inputs.join('\n') })
-      if(result.exitCode > 0) throw CompilationError(result.stderr);
-      return Utils.cleanStdout(result.stdout, promptsValues)
+      const data = {
+        starting_at: Date.now(),
+        source_code: content,
+      }
+      let result = await node.runSource(`${header} ${content}`, { stdin: inputs.join('\n') })
+      data.ended_at = Date.now();
+      result.stdout = Utils.cleanStdout(result.stdout, promptsValues);
+      result = { ...result, ...data }
+      // if(result.exitCode > 0) throw CompilationError(result.stderr);
+      return result
     }
     catch(e){
       throw CompilationError(e.message || e.msg);
--- src/test.js
+++ src/test.js
@@ -7,10 +7,10 @@ const { TestingError } = require('learnpack/plugin')
 const { getPrompts } = require("./utils");
 
 let nodeModulesPath = path.dirname(require.resolve('jest'))
-nodeModulesPath = nodeModulesPath.substr(0,nodeModulesPath.indexOf("node_modules")) + "node_modules/"
+nodeModulesPath = nodeModulesPath.substr(0, nodeModulesPath.indexOf("node_modules")) + "node_modules/"
 
-module.exports =  {
-  validate: async function({ exercise, configuration }){
+module.exports = {
+  validate: async function ({ exercise, configuration }) {
 
     if (!shell.which('jest')) {
       const packageName = "jest@24.8.0";
@@ -20,7 +20,7 @@ module.exports =  {
     return true
   },
   run: async ({ exercise, socket, configuration }) => {
-    
+
     let jestConfig = {
       verbose: true,
       moduleDirectories: [path.resolve(nodeModulesPath)],
@@ -31,57 +31,78 @@ module.exports =  {
     }
 
     const getEntry = () => {
-      
+
       let testsPath = exercise.files.map(f => f.path).find(f => f.includes('test.js') || f.includes('tests.js'));
-      if (!fs.existsSync(testsPath))  throw TestingError(`🚫 No test script found on the exercise files`);
-  
+      if (!fs.existsSync(testsPath)) throw TestingError(`🚫 No test script found on the exercise files`);
+
       return testsPath;
     }
 
-    const getCommands = async function(){
+    const getContent = () => {
+      const appPath = exercise.files.map(f => './' + f.path).find(f => f.includes(exercise.entry || 'app.js'));
+      if (!fs.existsSync(appPath)) throw TestingError(`🚫 No entry file found on the exercise files`);
+      return fs.readFileSync(appPath, "utf8");
+    }
 
-      const appPath = exercise.files.map(f => './'+f.path).find(f => f.includes(exercise.entry || 'app.js'));
+    const getCommands = async function () {
+
+      const appPath = exercise.files.map(f => './' + f.path).find(f => f.includes(exercise.entry || 'app.js'));
       let answers = []
-      if(appPath){
+      if (appPath) {
         const content = fs.readFileSync(appPath, "utf8");
         const promptsValues = getPrompts(content);
         answers = (promptsValues.length === 0) ? [] : await socket.ask(promptsValues);
       }
-      
-      jestConfig.reporters = [[ path.resolve(__dirname+'/utils/reporter.js'), { reportPath: path.resolve(`${configuration.dirPath}/reports/${exercise.slug}.json`) }]];
-      
+
+      jestConfig.reporters = [[path.resolve(__dirname + '/utils/reporter.js'), { reportPath: path.resolve(`${configuration.dirPath}/reports/${exercise.slug}.json`) }]];
+
       return `jest --config='${JSON.stringify({ ...jestConfig, globals: { __stdin: answers }, testRegex: getEntry() })}' --colors`
     }
 
     const getStdout = (rawStdout) => {
       let _stdout = [];
-      if (fs.existsSync(`${configuration.dirPath}/reports/${exercise.slug}.json`)){
+      if (fs.existsSync(`${configuration.dirPath}/reports/${exercise.slug}.json`)) {
         const _text = fs.readFileSync(`${configuration.dirPath}/reports/${exercise.slug}.json`);
         const errors = JSON.parse(_text);
-  
+
         _stdout = errors.testResults.map(r => r.message);
-  
-        if(errors.failed.length > 0){
-          msg = `\n\n   ${'Your code must to comply with the following tests:'.red} \n\n${[...new Set(errors.failed)].map((e,i) => `     ${e.status !== 'failed' ? chalk.green.bold('✓ (done)') : chalk.red.bold('x (fail)')} ${i}. ${chalk.white(e.title)}`).join('\n')} \n\n`;
+
+        if (errors.failed.length > 0) {
+          msg = `\n\n   ${'Your code must to comply with the following tests:'.red} \n\n${[...new Set(errors.failed)].map((e, i) => `     ${e.status !== 'failed' ? chalk.green.bold('✓ (done)') : chalk.red.bold('x (fail)')} ${i}. ${chalk.white(e.title)}`).join('\n')} \n\n`;
           _stdout.push(msg);
         }
       }
-      else throw TestingError("Could not find the error report for "+exercise.slug);
+      else throw TestingError("Could not find the error report for " + exercise.slug);
       return _stdout
     }
 
     let commands = await getCommands()
-    if(!Array.isArray(commands)) commands = [commands]
+    if (!Array.isArray(commands)) commands = [commands]
+
+    let appContent = getContent()
+    const result = {
+      starting_at: Date.now(),
+      source_code: appContent,
+    }
+
     let stdout, stderr, code = [null, null, null]
-    for(let cycle = 0; cycle < commands.length; cycle++){
+    for (let cycle = 0; cycle < commands.length; cycle++) {
       let resp = shell.exec(commands[cycle], { silent: true })
       stdout = resp.stdout
       code = resp.code
       stderr = resp.stderr
-      if(code != 0) break
+      if (code != 0) break
     }
 
-    if(code != 0) throw TestingError(getStdout(stdout || stderr).join())
-    else return stdout && stdout.length > 0 ? stdout : chalk.green("✔ All tests have passed")
+    result.ended_at = Date.now();
+    result.exitCode = code
+    result.stdout = stdout
+    result.stderr = stderr
+
+    if (code != 0) {
+      result.stderr = getStdout(stdout || stderr).join()
+    }
+    chalk.green("✔ All tests have passed")
+    return result
   }
 }
